211030422d72ee07b429f71ea617a1d48a250067
FeliciaSithole <feliciasithole.fs@gmail.com>
2025-08-01 13:11:43 +0200

    added the commands

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
-
+# Ask the user to enter the first number 
 num1 = float(input("Enter the first number: "))
 
+
+# Ask the user to enter the second number
 num2 = float(input("Enter the second number: "))
 
+# Ask the user to choose an operation
 operation = input("Enter the operation (+, -, *, /): ") 
 
+# Perform the chosen operations and display the result
 if operation == "+":
     result = num1 + num2
     print(f"{num1} + {num2} = {result}")
